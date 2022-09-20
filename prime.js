2e7044566764e4bfd12271ba2394e308f0e10395
Ravihattarki <rhme073@gmail.com>
2022-09-20 13:11:44 +0530

    code fixed by ravi

--- prime.js
+++ prime.js
@@ -1,7 +1,25 @@
 function check(num){
 
-let factor=0;
+let count=0;
 
 for(let i=0;i<=num;i++){
 if(num%i==0){
-factor++;
+count++;
+}
+}
+if(count==2)
+{
+return true;
+}
+return false
+}
+
+let ans=check(13);
+if(ans==true)
+{
+console.log("prime")
+}
+else
+{
+console.log("not prime");
+}
